ce3dd1aecf8388c5e7707b97e3e92bb348468ca7
Everson Magalhães <everson4774@gmail.com>
2021-09-29 16:09:37 -0300

    Starting project using react and typescript

---
+++ src/store/todos-context.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Todo from "../models/todo";
+
+type TodosContextOBj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+}
+
+export const TodosContext = React.createContext<TodosContextOBj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC = (props) => {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodo = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextOBj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodo,
+  };
+
+
+  return <TodosContext.Provider value={contextValue}>{props.children}</TodosContext.Provider>;
+};
+
+export default TodosContextProvider;
