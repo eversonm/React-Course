fe657b5d841afe1c2980f28f4290f7e5952df5bc
Everson Magalhães <everson4774@gmail.com>
2021-09-29 15:30:39 -0300

    Introduction to NextJS

---
+++ pages/news/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">NextJS</Link>
+        </li>
+        <li>NextJS 2</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
